9d27dc5f7099b55abcb031c1f58f073cd09c4304
dqarias <dino.quispea@gmail.com>
2022-05-23 16:27:18 -0500

    Add addData, renderData to the index.js

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
+//Create an array
+let bookArray = []
+
+//Declare form elements
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const formButton = document.querySelector('.button');
+
+const body = document.querySelector('body');
+const bookList = document.querySelector('.bookList');
+
+
+
+
+const form = document.querySelector('.form');
+
+
+//Create an addBook function
+function addData(e) {
+    e.preventDefault();
+    console.log("its working")
+    const titleValue = bookTitle.value;
+    const authorValue = bookAuthor.value;
+    let book = {titleValue, authorValue};
+    bookArray.push(book); 
+    console.log(bookArray);
+    body.insertBefore(bookList, form);
+    return bookArray;     
+}
+
+formButton.addEventListener('click', addData)
+
+function renderData(){
+    bookArray.forEach((book)=>{
+        const singleBook = document.createElement('div');
+        singleBook.classList.add('singleBook');
+        singleBook.innerHTML= ` 
+        <p class="book_title">${book.titleValue}</p>
+        <p class="book_author">${book.authorValue}</p>
+        <button class="removeButton">Remove</button>`
+        bookList.appendChild(singleBook);
+    })
+    
+}
+
+formButton.addEventListener('click', renderData)
+
+function removeData(){
+    bookArray.filter((book)=>{
+        
+    })
+}
+
+/* 
 const body = document.querySelector('body');
 const bookList = document.createElement('ul');
 bookList.classList.add('bookList');
@@ -10,22 +64,20 @@ const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const formButton = document.querySelector('.button');
 
+
+
 function storeData() {
-    const titleValue = bookTitle.value;
-    const authorValue = bookAuthor.value;
-   
-    const book = {titleValue, authorValue};
-   
+    
     if (bookTitle && bookAuthor) {
        const stringedBook = JSON.stringify(book);
        localStorage.setItem('book', stringedBook);
     }
-    bookArray.push(book);
    }
-   formButton.addEventListener('click', storeData);
+
+   //formButton.addEventListener('click', storeData);
 
 //Get data
-let bookArray = [];
+
 if (localStorage.getItem('book')) {
     let book = JSON.parse(localStorage.getItem('book'));
     let title = book.titleValue;
@@ -34,7 +86,7 @@ if (localStorage.getItem('book')) {
     console.log("Author: " + author);
 }
 
-
+ */
 
 
 /* let book = {bookTitle, bookAuthor}; */
